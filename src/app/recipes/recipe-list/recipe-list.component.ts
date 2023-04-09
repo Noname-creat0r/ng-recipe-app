96cc169aebd6109649a671fe14b42b88002d0f6b
Noname-creat0r <egorchik228322@gmail.com>
2023-04-09 12:20:16 +0300

    Selecting and loading recipe in details. Custom routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,9 +7,14 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test',
       'https://www.savingdessert.com/wp-content/uploads/2018/05/Spicy-Honey-Lime-Salmon-Recipe-5.jpg')
   ];
+ 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 
 }
