9de541701aac2c2f01cb996a86b0f7d07c0b9a0d
Noname-creat0r <egorchik228322@gmail.com>
2023-04-17 10:35:04 +0300

    Adding recipe edit page

---
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-start',
+  template: `
+    <h3 class="my-2">
+      Select a Recipe first!
+    </h3>
+  `
+})
+export class RecipeStartComponent {
+
+}
